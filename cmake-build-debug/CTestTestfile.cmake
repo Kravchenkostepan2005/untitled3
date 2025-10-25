a1a48bbec02f34208081cb20356fde06a58cde1e
Kravchenkostepan2005 <kravchenkostepan2005@gmail.com>
2025-10-25 12:56:58 +0200

    Ignore build artifacts

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/stepankravchenko/CLionProjects/untitled3
-# Build directory: /Users/stepankravchenko/CLionProjects/untitled3/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[unit_tests]=] "/Users/stepankravchenko/CLionProjects/untitled3/cmake-build-debug/unit_tests")
-set_tests_properties([=[unit_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/stepankravchenko/CLionProjects/untitled3/CMakeLists.txt;37;add_test;/Users/stepankravchenko/CLionProjects/untitled3/CMakeLists.txt;0;")
